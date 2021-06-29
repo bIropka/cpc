1d9318aea4b3613b0757496a7a3b8f133b05c81c
Pavlo Mykhailenko <snapeeye@gmail.com>
2021-06-29 11:38:08 +0300

    added mock table

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useMemo} from 'react';
+import {DataGrid} from "@material-ui/data-grid";
 import './App.css';
-import Table from '@material-ui/core/';
+import mock from './mock';
 
-function App() {
-  return (
-    <div className="App">
-      APP COMPONENT
-    </div>
-  );
+const App = () => {
+    const rows = useMemo(() => mock, []);
+    const columns = useMemo(() => ([
+            {field: 'customerName', headerName: 'CUSTOMER NAME', width: 300},
+            {field: 'parentCompany', headerName: 'PARENT COMPANY', width: 250, sortable: false},
+            {
+                field: 'billTo', headerName: 'BILL TO', width: 150, sortable: false,
+            },
+            {
+                field: 'accountBalanceFormat', headerName: 'ACCOUNT BALANCE (USD)', width: 200, sortable: false,
+                // @ts-ignore
+                valueGetter: (params) => new Intl.NumberFormat('en-US', {
+                    style: 'currency',
+                    currency: 'USD'
+                }).format(params.getValue(params.id, 'accountBalance')),
+            },
+            {field: 'lastInvoice', headerName: 'LAST INVOICE', width: 150, sortable: false},
+            {field: 'status', headerName: 'STATUS', width: 150, sortable: false},
+        ]), []);
+
+    return (
+        <div className="App">
+            <div className="Grid">
+                <DataGrid rows={rows} columns={columns} hideFooterPagination={true}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
