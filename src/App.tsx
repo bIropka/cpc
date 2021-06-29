7d11857de3f0e421637af36b4873024a4b5a00a1
Pavlo Mykhailenko <snapeeye@gmail.com>
2021-06-29 10:32:58 +0300

    added create react app from typescript template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Table from '@material-ui/core/';
+
+function App() {
+  return (
+    <div className="App">
+      APP COMPONENT
+    </div>
+  );
+}
+
+export default App;
