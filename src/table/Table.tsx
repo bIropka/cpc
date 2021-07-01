cb3e04d5222bea9bf857a82abce8983ef5bdd2fe
Pavlo Mykhailenko <snapeeye@gmail.com>
2021-07-01 11:07:08 +0300

    added table header items

--- src/table/Table.tsx
+++ src/table/Table.tsx
@@ -2,8 +2,12 @@ import React, {useMemo} from 'react';
 import {DataGrid, GridCellParams} from "@material-ui/data-grid";
 import {Link} from "react-router-dom";
 import Chip from '@material-ui/core/Chip';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import './Table.css';
 // @ts-ignore
 const Table = (props) => {
@@ -86,16 +90,43 @@ const Table = (props) => {
             headerAlign: 'left' as const,
             renderCell: (params: GridCellParams) => (
                 <IconButton aria-label="delete">
-                    <DeleteIcon />
+                    <DeleteIcon/>
                 </IconButton>
             ),
         },
     ]), []);
 
     return (
-        <div className="Grid-wrapper">
-            <div className="Grid">
-                <DataGrid autoHeight rows={rows} columns={columns} disableColumnMenu={true} hideFooter={true}  />
+        <div className="grid-wrapper">
+            <div className="grid-wrapper__grid-header grid-header">
+                <div className='grid-header__info-block header-info-block'>
+                    <Avatar>
+                        <svg className="MuiSvgIcon-root MuiAvatar-fallback" focusable="false" viewBox="0 0 24 24"
+                             aria-hidden="true">
+                            <path
+                                d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path>
+                        </svg>
+                    </Avatar>
+                    <span className='header-info-block__header-title'>{'Customer Accounts'}</span>
+                </div>
+                <div className='grid-header__functional-block header-functional-block'>
+                    <Button variant="outlined"
+                            color="primary"
+                            size="small"
+                            endIcon={<ArrowDropDownIcon />}
+                            className="header-functional-block__button-spacing">
+                        {'IMPORT ACCOUNTS'}
+                    </Button>
+                    <Button variant="contained"
+                            color="primary"
+                            size="small"
+                            startIcon={<AddIcon />}>
+                        {'CREATE NEW ACCOUNT'}
+                    </Button>
+                </div>
+            </div>
+            <div className="grid-wrapper__grid grid">
+                <DataGrid autoHeight rows={rows} columns={columns} disableColumnMenu={true} hideFooter={true}/>
             </div>
         </div>
     );
